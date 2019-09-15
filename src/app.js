7bd8acb2b37812619ac6abdbd3fea16843c2fc19
vinicius <viniciusfon96@gmail.com>
2019-09-15 20:29:55 -0300

    added file upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import routes from './routes';
 
 import './database';
@@ -7,7 +6,6 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-    this.server.use(cors);
     this.middlewares();
     this.routes();
   }
