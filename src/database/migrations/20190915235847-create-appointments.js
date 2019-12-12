256a9b3e8ba23db694de7a053b0bddff1841e733
vinicius fonseca <viniciusfon96@gmail.com>
2019-12-12 15:48:32 -0300

    added appointments and appointments validations

--- src/database/migrations/20190915235847-create-appointments.js
+++ src/database/migrations/20190915235847-create-appointments.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('appointents', {
+    return queryInterface.createTable('appointments', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -40,6 +40,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('appointents');
+    return queryInterface.dropTable('appointments');
   },
 };
