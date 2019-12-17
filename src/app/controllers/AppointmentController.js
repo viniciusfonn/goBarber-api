25d5b7149154c2222890b7dbdf96df2181e96b37
vinicius fonseca <viniciusfon96@gmail.com>
2019-12-17 16:55:19 -0300

    added appointment index method

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,13 @@ import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.user_id, canceled_at: null },
+    });
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
